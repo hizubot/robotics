5f47927b2f2fc829b2f10a47aeac144015a272da
hizubot <chris11996@hotmail.com>
2025-03-14 20:58:16 -0500

    activities template

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeTitle() {
+    document.getElementById("slide-title").style.display = "none";
+    document.getElementById("slide-device").style.display = "flex";
+}
+window.onload = function() {
+    setTimeout(changeTitle, 2000);
+};
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".device:nth-child(3)").addEventListener("click", function () {
+        document.getElementById("slide-device").style.display = "none";
+        document.getElementById("slide-activities").style.display = "flex";
+    });
+});
